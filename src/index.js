88349ea1d43d0ba6f10e9f7457f3dadbcf205fc0
Aljem <aljemmark.aviola@fligno.com>
2019-08-30 11:32:20 +0800

    add axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from '~pages/app/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from '~pages/app/App'
+import axios from 'axios'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+global.axios = axios
+
+// Set the axios baseURL here
+// axios.defaults.baseURL = 'http://52.221.234.188/api'
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
