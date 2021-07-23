e725e6536978b03b58eab7d2f7a5d8396d46b219
aljem-mark <speakout.aljem@gmail.com>
2021-07-24 00:29:23 +0800

    initial commit for new boilerplate

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require(`path`)
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@assets': path.resolve(__dirname, 'src/assets/')
+    }
+  }
+}
